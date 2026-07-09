a94154f23dc5e7cca76232d1f7dccc6bd5f9dab2
Anastasiia <anastasiia0matsiuk@gmail.com>
2026-07-09 21:31:31 +0300

    fix errorHandler

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,11 +1,15 @@
 // src/middleware/errorHandler.js
 
-export const errorHandler = (err, req, res, next) => {
-  console.error(err);
+import { HttpError } from 'http-errors';
 
-  const isProd = process.env.NODE_ENV === 'production';
+export const errorHandler = (err, req, res, next) => {
+  if (err instanceof HttpError) {
+    return res.status(err.status).json({
+      message: err.message,
+    });
+  }
 
   res.status(500).json({
-    message: isProd ? 'Internal Server Error' : err.message,
+    message: err.message,
   });
 };
