d6155f1d5bb8eb566187f2f16d6648b300058c66
Anastasiia <anastasiia0matsiuk@gmail.com>
2026-07-28 22:47:40 +0300

    add Register

--- src/server.js
+++ src/server.js
@@ -11,6 +11,7 @@ import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { connectMongoDB } from './db/connectMongoDB.js';
 
 import notesRoutes from './routes/notesRoutes.js';
+import authRoutes from './routes/authRoutes.js';
 
 dotenv.config();
 
@@ -20,6 +21,8 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(cors());
 app.use(logger);
+
+app.use(authRoutes);
 
 app.use(notesRoutes);
 
