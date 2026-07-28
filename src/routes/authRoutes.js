6f36ed982f9af0c46bcc021e27aabd9e44a0cc91
Anastasiia <anastasiia0matsiuk@gmail.com>
2026-07-28 23:45:02 +0300

    update Session

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -2,12 +2,13 @@
 
 import { Router } from 'express';
 import { celebrate } from 'celebrate';
-import { registerUser, loginUser } from '../controllers/authController';
-import { registerUserSchema, loginUserSchema } from '../validations/authValidation';
+import { registerUser, loginUser, refreshUserSession } from '../controllers/authController.js';
+import { registerUserSchema, loginUserSchema } from '../validations/authValidation.js';
 
 const router = Router();
 
 router.post('/auth/register', celebrate(registerUserSchema), registerUser);
 router.post('/auth/login', celebrate(loginUserSchema), loginUser);
+router.post('/auth/refresh', refreshUserSession);
 
 export default router;
